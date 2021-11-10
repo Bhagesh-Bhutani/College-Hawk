69a8e771286aff039aa932a31acceb78b1495992
Bhagesh-Bhutani <bhutanibhagesh49@gmail.com>
2021-11-11 01:34:51 +0530

    Deploy to heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 const expressLayouts = require('express-ejs-layouts');
 const sassMiddleware = require('node-sass-middleware');
 // Flash requires sessions, will do later
